797e6b84ae72e3372b623fa9b9621de0d332e38e
Rosana-Constantin <constantin.rosana@yahoo.com>
2018-05-06 23:31:55 +0300

    changes

--- index.js
+++ index.js
@@ -10,7 +10,6 @@ if (!databaseUri) {
 
 var api = new ParseServer({
     databaseURI: databaseUri || 'mongodb://localhost:27017/dev',
-    cloud: process.env.CLOUD_CODE_MAIN || __dirname + '/cloud/main.js',
     appId: process.env.APP_ID || 'myAppId',
     masterKey: process.env.MASTER_KEY || '',
     serverURL: process.env.SERVER_URL || 'http://localhost:1337/parse',
